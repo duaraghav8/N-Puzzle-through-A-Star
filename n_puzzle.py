1e250f603a264e2452da77c4eab1ad543cb20c5f
duaraghav8@gmail <duaraghav8@gmail.com>
2015-10-08 14:38:03 +0530

    Initial Commit

---
+++ n_puzzle.py
@@ -0,0 +1,73 @@
+from heapq import heapify, heappush, heappop;
+def hamming (dim, grid, target):
+	counter = 0;
+	for i in range (dim):
+		for j in range (dim):
+			if (not (grid [i] [j] == target [i] [j] or grid [i] [j] == 0)):
+				counter += 1;
+	return (counter);
+
+def getNextStates (dim, current):
+	nextStates = [];
+	empty = None;
+
+	for i in range (dim):
+		try:
+			empty = current [i].index (0);
+		except Exception as e:
+			continue;
+		empty = (i, empty);
+		break;
+
+	if (empty [1] < (dim - 1)):
+		a = [ i.copy () for i in current ];
+		a [empty [0]] [empty [1]], a [empty [0]] [empty [1] + 1] = a [empty [0]] [empty [1] + 1], a [empty [0]] [empty [1]];
+		nextStates.append (('RIGHT', a, (empty [0], empty [1] + 1)));
+
+	if (empty [1] > 0):
+		b = [ i.copy () for i in current ];
+		b [empty [0]] [empty [1]], b [empty [0]] [empty [1] - 1] = b [empty [0]] [empty [1] - 1], b [empty [0]] [empty [1]];
+		nextStates.append (('LEFT', b, (empty [0], empty [1] - 1)));
+
+	if (empty [0] > 0):
+		c = [ i.copy () for i in current ];
+		c [empty [0]] [empty [1]], c [empty [0] - 1] [empty [1]] = c [empty [0] - 1] [empty [1]], c [empty [0]] [empty [1]];
+		nextStates.append (('UP', c, (empty [0] - 1, empty [1])));
+
+	if (empty [0] < (dim - 1)):
+		d = [ i.copy () for i in current ];
+		d [empty [0]] [empty [1]], d [empty [0] + 1] [empty [1]] = d [empty [0] + 1] [empty [1]], d [empty [0]] [empty [1]];
+		nextStates.append (('DOWN', d, (empty [0] + 1, empty [1])));
+
+	return (nextStates);
+
+def getSequenceInfo (dim, grid):
+	target = [ [j for j in range (i, i + dim)] for i in range (0, (dim * (dim - 1)) + 1, dim) ];
+	current = (hamming (dim, grid, target), 0, [], grid)
+	stateTree = [current];
+
+	heapify (stateTree);
+	while (not current [-1] == target):
+		current = heappop (stateTree);
+#		print (current);
+
+		for state in getNextStates (dim, current [-1]):
+			heappush (stateTree, (hamming (dim, state [1], target) + current [1] + 1, current [1] + 1, current [2] + [state [0]], state [1]));
+
+	return (current [1], current [2]);
+
+dim = int (input ());
+row = numCount = 0;
+grid = [[] for i in range (dim)];
+
+for i in range (dim ** 2):
+	grid [row].append (int (input ()));
+	numCount += 1;
+
+	if (numCount % dim == 0):
+		row += 1;
+		numCount = 0;
+
+seqCount, sequence = getSequenceInfo (dim, grid);
+print (seqCount);
+print ('\n'.join (sequence));
